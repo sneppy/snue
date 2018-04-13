1c3a2f986022b49ccf5d6c297be0953c171353e2
Sneppy <andrea.mecchia@mail.polimi.it>
2018-04-13 19:52:34 +0200

    Added card component draft

---
+++ src/components/Card.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="card">
+		<h2 class="title" v-if="title !== undefined">{{ title }}</h2>
+		<slot/>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "card",
+	props: {
+		title: String
+	}
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 // Import components
+import Card			from "./components/Card"
 import Drawer		from "./components/Drawer"
 import LineGraph	from "./components/LineGraph"
 import Notification	from "./components/Notification"
 import Position		from "./components/Position"
 const components = [
+	Card,
 	Drawer,
 	LineGraph,
 	Notification,
