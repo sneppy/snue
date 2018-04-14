052227fecf4c5bbeb812fcca06793f388dcaf7d9
Sneppy <andrea.mecchia@mail.polimi.it>
2018-04-14 12:12:59 +0200

    Fixed stack non sizing down using range

--- src/components/Stack.vue
+++ src/components/Stack.vue
@@ -11,32 +11,49 @@
 export default {
 	name: "stack",
 	methods: {
+		/**
+		 * Calculate width and height of the stack
+		 */
 		render() {
 
 			const el		= this.$el;
 			const layers	= el.children;
-			
+
+			// Using a range ensure that only true content size is evaluated
+			const range = document.createRange();				
 			let maxW = 0, maxH = 0;
 			for (const layer of layers)
 			{
-				maxW = layer.offsetWidth > maxW ? layer.offsetWidth : maxW;
-				maxH = layer.offsetHeight > maxH ? layer.offsetHeight : maxH;
+				range.selectNodeContents(layer);
+
+				const width		= range.getBoundingClientRect().width;
+				const height	= range.getBoundingClientRect().height;
+				maxW = width > maxW ? width : maxW;
+				maxH = height > maxH ? height : maxH;
 			}
 
 			// Set height and width
 			this.$el.style.width	= maxW+"px";
-			this.$el.style.height	= maxH+"px";
+			this.$el.style.height	= maxH+"px";	
 		}
 	},
 	mounted() {
 
 		// Render stack
-		this.$nextTick(() => this.render());
+		this.render();
+
+		// Register resize event
+		window.addEventListener("resize", () => this.render());
 	},
 	updated() {
 
 		// Render stack
 		this.render();
+	},
+	destroyed() {
+
+		// Remove window listener
+		window.removeEventListener("resize", () => this.render());
 	}
 }
 </script>
